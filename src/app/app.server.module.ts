52e9e397ffa6b6e8d749a046ecabdcc34ac90890
Manish Gupta <manish@offo.app>
2022-11-27 16:18:16 +0530

    u

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,11 +4,11 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
-// import { FlexLayoutServerModule } from '@angular/flex-layout/server';
+import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 
 @NgModule({
   imports: [AppModule, ServerModule,
-    // FlexLayoutServerModule,
+    FlexLayoutServerModule,
   ],
   bootstrap: [AppComponent],
 })
